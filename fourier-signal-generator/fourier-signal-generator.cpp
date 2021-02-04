dfcc75e082d80af671fd8913f1b83d47e7413484
Konstantysz <kostekszumigaj@gmail.com>
2021-02-04 16:12:30 +0100

    - Set up project files

---
+++ fourier-signal-generator/fourier-signal-generator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+
+int main()
+{
+    cv::Mat xd;
+    std::cout << "Hello World!\n";
+
+    return 0;
+}
